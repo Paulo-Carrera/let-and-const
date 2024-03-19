e1843239dbd768d4bf8b35109ec4ae169eae7b34
Paulo <paulo3252@myyahoo.com>
2024-03-18 19:38:21 -0700

    forgot to press save

---
+++ app.js
@@ -0,0 +1,44 @@
+
+
+// var PI = 3.14 ;
+
+const PI = 3.14 ;
+
+PI = 42 ;
+
+
+//  To make sure the PI variable is NOT reassignable , we can use CONST
+
+
+
+
+//  QUIZ
+
+//      what is the difference between VAR and LET ? 
+
+
+//              VAR can be reassigned , redeclared and mutated , whereas LET cannot be redeclared and is block scope .
+
+
+
+
+//      what is the difference between VAR and CONST ?
+
+
+//              CONST cannot be reassigned or redeclared like VAR , also VAR is function scope while CONST is block scope .
+
+
+
+
+//      what is the difference between LET and CONST ?
+
+
+//              LET can be reassigned 
+
+
+
+
+//      What is hoisting ?
+
+
+//              hoisting is the boundaries variables have when it comes to being compiled , and is not as much of an issue with LET or CONST .   
